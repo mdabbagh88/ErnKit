4a0e4cfc7cf06fc26308d432e2f531f359c503bf
Magnus Ernstsson <magnus@ernstsson.net>
2013-06-30 08:19:39 +0100

    Changed base observable to ensure that all clients get notifications in their own thread

--- Source/ErnKit/Utilities/ERNBaseObservable.m
+++ Source/ErnKit/Utilities/ERNBaseObservable.m
@@ -3,10 +3,12 @@
 
 @interface ERNBaseObservable ()
 @property (nonatomic, readonly) NSNotificationCenter *notificationCenter;
+@property (nonatomic, readonly) NSMapTable *observers;
 @end
 
 @implementation ERNBaseObservable {
     NSNotificationCenter *_notificationCenter;
+    NSMapTable *_observers;
 }
 
 #pragma mark - public
@@ -18,23 +20,32 @@
 }
 #pragma mark - ERNObservable
 
--(void)addObserver:(id)observer
+-(void)addObserver:(id<NSObject>)observer
           selector:(SEL)selector
 {
     ERNCheckNilNoReturn(observer);
     ERNCheckNilNoReturn(selector);
-    [[self notificationCenter] addObserver:observer
-                                  selector:selector
-                                      name:[self notificationName]
-                                    object:self];
+    [[self observers] setObject:
+     [[self notificationCenter] addObserverForName:[self notificationName]
+                                           object:self
+                                            queue:[NSOperationQueue currentQueue]
+                                       usingBlock:^(NSNotification *note) {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+                                           [observer performSelector:selector];
+#pragma clang diagnostic pop
+                                       }]
+                         forKey:observer];
 }
+
 
 -(void)removeObserver:(id)observer
 {
     ERNCheckNilNoReturn(observer);
-    [[self notificationCenter] removeObserver:observer
+    [[self notificationCenter] removeObserver:[[self observers] objectForKey:observer]
                                          name:[self notificationName]
                                        object:self];
+    [[self observers] removeObjectForKey:observer];
 }
 
 #pragma mark - private
@@ -58,6 +69,11 @@
     return _notificationCenter = _notificationCenter ?
     _notificationCenter :
     [NSNotificationCenter new];
+}
+
+-(NSMapTable *)observers
+{
+    return _observers = _observers ? _observers : [NSMapTable strongToStrongObjectsMapTable];
 }
 
 @end
